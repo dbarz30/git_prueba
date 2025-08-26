24eaed09c0b6cbecac2dff366ac21adf9b71dd48
Dayra Barboza <dayra.barboza@utec.edu.pe>
2025-08-25 19:46:51 -0500

    Nuevo par de claves

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Primer cambio : Crear un archivo main.py
 Seguno Cambio: Añadir linea al archivo main.py
-
+Tercer cambio: Par de claves SSH creadas
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Hello World")
 print("Otro cambio para el tercer commit de mi repositorio con Git")
+print("Ya tengo mi par de claves")
 print("Este es mi priemr proyecto con git")
